1006fffe0532c966e7c6e87e206a935897558efd
Uttam <utam.shrestha65@gmail.com>
2023-01-22 12:04:56 +0545

    tested and works well

---
+++ sum.c
@@ -0,0 +1,10 @@
+// 1. WAP to display Sum of two numbers
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter two numbers to find the sum: ");
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    printf("The sum of two numbers is %d", sum);
+}
